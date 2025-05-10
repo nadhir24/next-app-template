71f00c0e4f71840cb63d6d9f11d9bd65f4a7567c
bbimabluu <bbimabluu@gmail.com>
2025-05-10 20:05:57 +0700

    feat: fix head config site.ts

--- config/site.ts
+++ config/site.ts
@@ -23,17 +23,12 @@ export const siteConfig = {
   ],
   navMenuItems: [
     {
-<<<<<<< HEAD
       label: "My Dasboard",
       href: "/dashboard",
     },
     {
       label: "Katalog",
       href: "/katalog",
-=======
-      label: "Profile",
-      href: "/profile",
->>>>>>> 77f85158d758c5ddc80273101a0ba52b5035df76
     },
     {
       label: "Invoice",
@@ -43,31 +38,10 @@ export const siteConfig = {
       label: "tentang kami",
       href: "/tentangkami",
     },
-<<<<<<< HEAD
   ],
   
   links: {
     google: "https://maps.app.goo.gl/uehWLXqC3s1z1Djw9",
     whatsapp: "https://wa.me/08151831185",
-=======
-    {
-      label: "Katalog",
-      href: "/Katalog",
-    },
-  ],
-  footerItems: [
-    {
-      label: "tentang kami",
-      href: "/tentangkami",
-    },
-    {
-      label: "Katalog",
-      href: "/Katalog",
-    },
-  ],
-  links: {
-    google: "https://maps.app.goo.gl/uehWLXqC3s1z1Djw9",
-    whatsapp: "https://wa.me/081385642024",
->>>>>>> 77f85158d758c5ddc80273101a0ba52b5035df76
   },
 };
