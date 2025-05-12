f7f08b741c0419801ccbd125ad92b0b135578a40
bbimabluu <bbimabluu@gmail.com>
2025-05-12 16:04:25 +0700

    Simplify image formats to just image/webp

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
     // Optimize image loading
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ["image/webp", "image/avif"],
+    formats: ["image/webp"],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
     disableStaticImages: false,
