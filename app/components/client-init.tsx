311b78a5180756538f045e43e9fcc457d0d5a1ba
bbimabluu <bbimabluu@gmail.com>
2025-05-14 16:49:01 +0700

    custom changes

--- app/components/client-init.tsx
+++ app/components/client-init.tsx
@@ -6,15 +6,7 @@ import { resetCartIfNeeded } from '../_app';
 // Client-side component untuk menginisialisasi reset cart otomatis
 export default function ClientInit() {
   useEffect(() => {
-    const initApp = async () => {
-      try {
-        await resetCartIfNeeded();
-      } catch (error) {
-        console.error("Failed to initialize app:", error);
-      }
-    };
-    
-    initApp();
+    resetCartIfNeeded();
   }, []);
   
   return null;
