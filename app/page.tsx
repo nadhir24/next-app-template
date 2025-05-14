b477f2b5fdcd9147964cd9ca06a7850823e9d36f
bbimabluu <bbimabluu@gmail.com>
2025-05-14 17:38:47 +0700

    minor and di asinan

--- app/page.tsx
+++ app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
       image: wkwkwkwk,
       title: "Nasi and Asinan",
       subTitle: "Fresh",
-      description: "Menu nasi dan asinan segar untuk acara Anda",
+      description: "Menu nasi dan asinan segar untuk acara apapun",
     },
   ];
 
